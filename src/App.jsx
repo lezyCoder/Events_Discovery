9d7cba3ada685aa003afc1038646011958dce5a9
lezyCoder <iamvijaysingh23@gmail.com>
2026-01-26 22:41:10 +0530

    feat: implement Firebase database integration and add data submission functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import ChangeText from "./ChangeText.jsx"
+import { ref, set, getDatabase } from "firebase/database"
+import { app } from "./Firebase/Config"
+
+
+const db = getDatabase(app)
 
 const App = () => {
 
+  const putData = () => {
+    set(ref(db, "users/vijay"), {
+      id: 1,
+      name: "Vijay",
+      age: 26,
+    })
+  }
+
   return (
     <div>
-      App
+      <button onClick={putData}>Put data </button>
     </div>
   )
 }
