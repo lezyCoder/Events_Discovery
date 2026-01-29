9b9b4a7ed3371759d82bbae47d7b45f79f7cf0f7
lezyCoder <iamvijaysingh23@gmail.com>
2026-01-29 17:38:52 +0530

    feat: update event fetching to use proxy configuration and improve API request structure

--- src/Axios/Axios.js
+++ src/Axios/Axios.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 
 const fetchEvents = async () => {
-  const options = {
-    method: "get",
-    url: `https://app.ticketmaster.com/discovery/v1/events.json?apikey=${import.meta.env.VITE_DISCOVERY_API}`,
+  const response = await axios.get(`/api/discovery/v2/events.json`, {
+    params: {
+      apikey: import.meta.env.VITE_DISCOVERY_API,
+    },
     headers: {
       accept: "application/json",
     },
-  };
-  const response = await axios(options);
+  });
 
-  console.log("axios response", response);
-  return response.data 
+  console.log("axios response", response.data);
+  return response.data;
 };
 
 export default fetchEvents;
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss()],  server: {
+    proxy: {
+      '/api': {
+        target: 'https://app.ticketmaster.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 });
