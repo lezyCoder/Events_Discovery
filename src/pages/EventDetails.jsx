08ce61ab56da4cfee825825c3a1ac4faa66e2e9d
lezyCoder <iamvijaysingh23@gmail.com>
2026-02-03 06:56:57 +0530

    feat: : working on details page

--- src/pages/EventDetails.jsx
+++ src/pages/EventDetails.jsx
@@ -3,10 +3,17 @@ import { useParams } from "react-router"
 const EventDetails = () => {
 
   const id = useParams()
-
   return (
     <div>
-      <p>Events Details</p>
+      <div className="container grid grid-cols-2 gap-4 w-full bg-amber-100">
+        <div className="left ">
+          <img src="" alt="poster here" />
+        </div>
+        <div className="right border">
+          <p>Title </p>
+          <p className="infor">Information</p>
+        </div>
+      </div>
     </div>
   )
 }
