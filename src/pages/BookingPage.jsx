4cc765209f83d988dc913726f0c6bf2fa1f66f64
lezyCoder <iamvijaysingh23@gmail.com>
2026-02-18 19:53:33 +0530

    feat: add useLocation hook to BookingPage for accessing route state

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,7 +1,12 @@
+import { useLocation } from "react-router"
 
 
 
 const BookingPage = () => {
+
+    const location = useLocation()
+    const { state } = location;
+    // console.log("data :", state)
     return (
         <div className="flex items-center justify-center">
             Booking details
