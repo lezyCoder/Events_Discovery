05de0fcd2ace55a2c5338e214dc85f0a9cece28e
lezyCoder <iamvijaysingh23@gmail.com>
2026-02-19 20:27:20 +0530

    feat: enhance BookingPage layout and display booking details with improved structure

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -6,10 +6,17 @@ const BookingPage = () => {
 
     const location = useLocation()
     const { state } = location;
-    // console.log("data :", state)
+    console.log("data :", state)
+    const {data} = state;
     return (
-        <div className="flex items-center justify-center">
-            Booking details
+        <div className="booking-container flex items-center justify-center">
+            
+            <h1>Booking Details </h1>
+
+            <div className="booking-details">
+                <img src={`${data.img}` }alt="data.title" />
+                <p>{data.title}</p>
+            </div>
         </div>
     )
 }
